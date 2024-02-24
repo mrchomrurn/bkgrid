669553c40a9ddc63db0f93f4eefa539ef6f4254e
Chomrurn Maing <maingchomrurn76@gmail.com>
2024-02-24 16:18:49 +0700

    chore: bump(ag-grid-community) 31.0.3 to 31.1.1

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,14 @@
-import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import dts from "vite-plugin-dts";
 import path from "path";
+import { defineConfig } from "vite";
+import dts from "vite-plugin-dts";
 import libCss from "vite-plugin-libcss";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  define: {
+    __VUE_PROD_HYDRATION_MISMATCH_DETAILS__: 'true',
+  },
   plugins: [vue(), dts(), libCss()],
   build: {
     lib: {
